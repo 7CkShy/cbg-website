6fff2f6c0f40134003a27fa6717bf30288e26cec
Kai Cai <129074869+7CkShy@users.noreply.github.com>
2026-03-07 22:44:20 +0800

    Switch from BrowserRouter to HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,8 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
